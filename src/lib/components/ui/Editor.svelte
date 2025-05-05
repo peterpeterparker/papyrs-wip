c6164d12e47ea9cb4ba969629500fd2b3305d86d
David Dal Busco <david.dalbusco@outlook.com>
2025-05-05 15:19:16 +0200

    feat: init editor with tiptap

---
+++ src/lib/components/ui/Editor.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Editor } from '@tiptap/core';
+	import type { Markdown } from '$lib/types/core';
+	import EditorContent from '$lib/components/ui/EditorContent.svelte';
+
+	export let content: Markdown;
+	export let onUpdate: (json: Markdown) => Promise<void>;
+
+	let editor: Editor | undefined;
+</script>
+
+<div>
+	<EditorContent bind:editor {content} {onUpdate} />
+</div>
