856538455ccd88b89b46bc0a3c4640116e9faeb6
David Dal Busco <david.dalbusco@outlook.com>
2025-05-12 14:30:47 +0200

    chore: scss warnings

--- src/lib/components/ui/EditorContent.svelte
+++ src/lib/components/ui/EditorContent.svelte
@@ -45,11 +45,9 @@
 <article bind:this={element}></article>
 
 <style lang="scss">
-	@use '../../styles/mixins/stylo';
 	@use '../../styles/mixins/doc';
 
 	article {
-		@include stylo.editor;
 		@include doc.article;
 		@include doc.placeholder;
 
