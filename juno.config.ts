56248cd725b5580ce09271b1bdf0e57aea5d8b9a
David Dal Busco <david.dalbusco@outlook.com>
2025-08-11 15:19:28 +0200

    feat: use Satellite emulator image and config collections

--- juno.config.ts
+++ juno.config.ts
@@ -1,12 +1,50 @@
-import { defineConfig } from '@junobuild/config';
+import { defineConfig, type Collections } from '@junobuild/config';
+
+const collections: Collections = {
+	datastore: [
+		{
+			collection: 'metadata',
+			read: 'managed',
+			write: 'managed',
+			memory: 'stable'
+		},
+		{
+			collection: 'content',
+			read: 'managed',
+			write: 'managed',
+			memory: 'stable'
+		}
+	],
+	storage: [
+		{
+			collection: 'images',
+			read: 'managed',
+			write: 'managed',
+			memory: 'stable'
+		},
+		{
+			collection: 'content',
+			read: 'managed',
+			write: 'managed',
+			memory: 'stable'
+		}
+	]
+};
 
 export default defineConfig({
 	satellite: {
 		ids: {
-			development: 'bkyz2-fmaaa-aaaaa-qaaaq-cai',
-			production: 'zry52-liaaa-aaaal-artyq-cai'
+			development: 'jx5yt-yyaaa-aaaal-abzbq-cai',
+			production: '<PROD_SATELLITE_ID>'
 		},
 		source: 'build',
-		predeploy: ['npm run build']
+		predeploy: ['npm run build'],
+		collections
+	},
+	emulator: {
+		runner: {
+			type: "docker"
+		},
+		satellite: {}
 	}
 });
