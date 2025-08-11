4a7b0e3609b5175180129b823ca556185b9b62ed
David Dal Busco <david.dalbusco@outlook.com>
2025-08-11 15:43:31 +0200

    feat: generate static template

---
+++ src/routes/(blog)/template/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<article>%%PLACEHOLDER%%</article>
+
+<style lang="scss">
+  @use '../../../lib/styles/mixins/doc';
+
+  article {
+    @include doc.article;
+    @include doc.placeholder;
+  }
+</style>
